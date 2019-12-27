412c99b097eb2ab80698812dfbcc39e5317478d4
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-27 21:58:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,22 @@
 def map(array)
 
 	array.map { |n| n * -1 }
-	array.map { |n| n  }
-	array.map { |n| n * 2 } 
-	array.map { |n| n * n }
+	
+	
+	
 end
 
+def map(array)
+	array.map { |n| n  }
+end
+
+
+def map(array)
+
+	array.map { |n| n * 2 } 
+
+end
+
+def map(array)
+	array.map { |n| n * n }
+end
