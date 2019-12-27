e0b5b4bf8f75339c941243cfee75832e35d71200
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-27 22:18:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,9 @@
 def map(array)
 
-	array.map { |n| n * -1 }
-	
+	negative = array.map { |n| n * -1 }
+	original = negative.map { |n| n * -1}
+	multiplied_by_2 = original.map { |n| n * 2}
+	array.map { |n| n * n  }
 	
 	
 end
